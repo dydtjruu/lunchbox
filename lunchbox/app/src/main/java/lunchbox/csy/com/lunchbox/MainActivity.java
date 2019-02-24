72604d7bb806572f578389348dbc87ed9cdca645
ckh0211a <ckh0211a@naver.com>
2019-02-24 17:25:37 +0900

    aaa
    aaa

---
+++ lunchbox/app/src/main/java/lunchbox/csy/com/lunchbox/MainActivity.java
@@ -0,0 +1,13 @@
+package lunchbox.csy.com.lunchbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
