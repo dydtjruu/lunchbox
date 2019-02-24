b0bec86d77eed5fe615e50d17a30913f553776fe
ckh0211a <ckh0211a@naver.com>
2019-02-24 17:29:09 +0900

    20190224 버튼수정
    20190224 버튼수정 자세하게

--- lunchbox/app/src/main/java/lunchbox/csy/com/lunchbox/MainActivity.java
+++ lunchbox/app/src/main/java/lunchbox/csy/com/lunchbox/MainActivity.java
@@ -1,13 +1,16 @@
 package lunchbox.csy.com.lunchbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);  
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
     }
 }
