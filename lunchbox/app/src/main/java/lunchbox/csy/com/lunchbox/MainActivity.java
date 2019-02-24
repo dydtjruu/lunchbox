899b1d7376c295564d24e58d93989ccdacd101a2
kimgaram0718 <kimgaram0718@naver.com>
2019-02-24 23:26:09 +0900

    garam push test

--- lunchbox/app/src/main/java/lunchbox/csy/com/lunchbox/MainActivity.java
+++ lunchbox/app/src/main/java/lunchbox/csy/com/lunchbox/MainActivity.java
@@ -3,6 +3,7 @@ package lunchbox.csy.com.lunchbox;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+        Toast.makeText(this,"garam push test",Toast.LENGTH_LONG).show();
     }
 }
